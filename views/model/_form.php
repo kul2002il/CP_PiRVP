4631ebc596f522d3db7dde415de65fcbc4f3f815
Ilia <kul.2002.il@gmail.com>
2021-06-06 20:55:08 +0700

    CRUD для моделей аппаратов.

---
+++ views/model/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Model */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="model-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'idType')->textInput() ?>
+
+    <?= $form->field($model, 'idBrand')->textInput() ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
