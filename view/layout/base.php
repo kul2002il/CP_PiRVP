f6d53502b462cf8f96fc302acd6cea3bce0adad1
Ilia <kul.2002.il@gmail.com>
2021-08-08 16:36:30 +0700

    Макет v0.2 своими силами.

--- view/layout/base.php
+++ view/layout/base.php
@@ -3,72 +3,38 @@
 <head>
 	<meta charset="UTF-8">
 	<title>РЕПРОТЭК</title>
-	<link rel="stylesheet" type="text/css" href="static/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/static/css/style.css">
 </head>
 <body>
-	<div>
-		<div class="left_menu">
-			<div class="header">
-				<h2>Меню</h2>
-			</div>
-			<nav>
-				<ul>
-					<li>
+	<header>
+		<div class="container">
+			<h1>РEПРОТЭК</h1>
+			<div class="dropdown_menu">
+				<div>Меню</div>
+				<div>
+					<nav>
 						<a href="?target=main">Главная</a>
-					</li>
-					<li>
 						<a href="?target=news">Новости</a>
-					</li>
-					<li>
 						<a href="?target=contact">Контакты</a>
-					</li>
-					<li>
 						<a href="?target=about">О нас</a>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	</div>
-	<main>
-		<div class="content">
-			<div class="header">
-				<h1>РЕПРОТЭК</h1>
+					</nav>
+				</div>
 			</div>
+		</div>
+	</header>
+	<main>
+		<div class="content container">
 			<article>
 				<h3>Lorem ipsum</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eleifend est dui, sed elementum est consequat in. Morbi neque metus, porta quis dolor malesuada, condimentum elementum est. Cras et ipsum sed augue sollicitudin sagittis ut id libero. Aenean nec commodo felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis eget felis eu elit scelerisque hendrerit. Proin eros mauris, sollicitudin et lobortis a, auctor in nibh. Phasellus finibus finibus facilisis. Praesent ipsum erat, tempus eu ante at, pulvinar vestibulum elit. Morbi egestas ligula a placerat porta. Ut fringilla posuere diam vitae rhoncus. Vestibulum at posuere quam. Morbi quis lectus egestas, sagittis tortor ut, consectetur est. Mauris lorem tellus, hendrerit vel ligula id, aliquam tincidunt eros. Nullam pellentesque gravida nisi, non tincidunt quam.
-				</p>
-				<p>
-					Nullam aliquam mollis ipsum, in pellentesque lorem commodo eget. Sed lobortis est eget sollicitudin dapibus. Aenean dolor turpis, rhoncus sit amet est a, hendrerit faucibus magna. Nulla tortor erat, hendrerit ac varius at, vestibulum id nisi. Nam dapibus pharetra gravida. Proin non tincidunt sapien. Mauris dapibus tempus sollicitudin. In fermentum a ipsum eget aliquam. Nulla rhoncus lorem in leo scelerisque euismod. Aliquam lacinia tempor justo et posuere. In ac velit mattis, vulputate velit vel, dictum orci. Sed ac finibus massa.
-				</p>
-			</article>
-			<article>
-				<h3>Lorem ipsum</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eleifend est dui, sed elementum est consequat in. Morbi neque metus, porta quis dolor malesuada, condimentum elementum est. Cras et ipsum sed augue sollicitudin sagittis ut id libero. Aenean nec commodo felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis eget felis eu elit scelerisque hendrerit. Proin eros mauris, sollicitudin et lobortis a, auctor in nibh. Phasellus finibus finibus facilisis. Praesent ipsum erat, tempus eu ante at, pulvinar vestibulum elit. Morbi egestas ligula a placerat porta. Ut fringilla posuere diam vitae rhoncus. Vestibulum at posuere quam. Morbi quis lectus egestas, sagittis tortor ut, consectetur est. Mauris lorem tellus, hendrerit vel ligula id, aliquam tincidunt eros. Nullam pellentesque gravida nisi, non tincidunt quam.
-				</p>
-				<p>
-					Nullam aliquam mollis ipsum, in pellentesque lorem commodo eget. Sed lobortis est eget sollicitudin dapibus. Aenean dolor turpis, rhoncus sit amet est a, hendrerit faucibus magna. Nulla tortor erat, hendrerit ac varius at, vestibulum id nisi. Nam dapibus pharetra gravida. Proin non tincidunt sapien. Mauris dapibus tempus sollicitudin. In fermentum a ipsum eget aliquam. Nulla rhoncus lorem in leo scelerisque euismod. Aliquam lacinia tempor justo et posuere. In ac velit mattis, vulputate velit vel, dictum orci. Sed ac finibus massa.
-				</p>
-			</article>
-			<article>
-				<h3>Lorem ipsum</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eleifend est dui, sed elementum est consequat in. Morbi neque metus, porta quis dolor malesuada, condimentum elementum est. Cras et ipsum sed augue sollicitudin sagittis ut id libero. Aenean nec commodo felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis eget felis eu elit scelerisque hendrerit. Proin eros mauris, sollicitudin et lobortis a, auctor in nibh. Phasellus finibus finibus facilisis. Praesent ipsum erat, tempus eu ante at, pulvinar vestibulum elit. Morbi egestas ligula a placerat porta. Ut fringilla posuere diam vitae rhoncus. Vestibulum at posuere quam. Morbi quis lectus egestas, sagittis tortor ut, consectetur est. Mauris lorem tellus, hendrerit vel ligula id, aliquam tincidunt eros. Nullam pellentesque gravida nisi, non tincidunt quam.
-				</p>
-				<p>
-					Nullam aliquam mollis ipsum, in pellentesque lorem commodo eget. Sed lobortis est eget sollicitudin dapibus. Aenean dolor turpis, rhoncus sit amet est a, hendrerit faucibus magna. Nulla tortor erat, hendrerit ac varius at, vestibulum id nisi. Nam dapibus pharetra gravida. Proin non tincidunt sapien. Mauris dapibus tempus sollicitudin. In fermentum a ipsum eget aliquam. Nulla rhoncus lorem in leo scelerisque euismod. Aliquam lacinia tempor justo et posuere. In ac velit mattis, vulputate velit vel, dictum orci. Sed ac finibus massa.
-				</p>
-			</article>
-			<article>
-				<h3>Lorem ipsum</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eleifend est dui, sed elementum est consequat in. Morbi neque metus, porta quis dolor malesuada, condimentum elementum est. Cras et ipsum sed augue sollicitudin sagittis ut id libero. Aenean nec commodo felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis eget felis eu elit scelerisque hendrerit. Proin eros mauris, sollicitudin et lobortis a, auctor in nibh. Phasellus finibus finibus facilisis. Praesent ipsum erat, tempus eu ante at, pulvinar vestibulum elit. Morbi egestas ligula a placerat porta. Ut fringilla posuere diam vitae rhoncus. Vestibulum at posuere quam. Morbi quis lectus egestas, sagittis tortor ut, consectetur est. Mauris lorem tellus, hendrerit vel ligula id, aliquam tincidunt eros. Nullam pellentesque gravida nisi, non tincidunt quam.
-				</p>
-				<p>
-					Nullam aliquam mollis ipsum, in pellentesque lorem commodo eget. Sed lobortis est eget sollicitudin dapibus. Aenean dolor turpis, rhoncus sit amet est a, hendrerit faucibus magna. Nulla tortor erat, hendrerit ac varius at, vestibulum id nisi. Nam dapibus pharetra gravida. Proin non tincidunt sapien. Mauris dapibus tempus sollicitudin. In fermentum a ipsum eget aliquam. Nulla rhoncus lorem in leo scelerisque euismod. Aliquam lacinia tempor justo et posuere. In ac velit mattis, vulputate velit vel, dictum orci. Sed ac finibus massa.
-				</p>
+				<div>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eleifend est dui, sed elementum est consequat in. Morbi neque metus, porta quis dolor malesuada, condimentum elementum est. Cras et ipsum sed augue sollicitudin sagittis ut id libero. Aenean nec commodo felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis eget felis eu elit scelerisque hendrerit. Proin eros mauris, sollicitudin et lobortis a, auctor in nibh. Phasellus finibus finibus facilisis. Praesent ipsum erat, tempus eu ante at, pulvinar vestibulum elit. Morbi egestas ligula a placerat porta. Ut fringilla posuere diam vitae rhoncus. Vestibulum at posuere quam. Morbi quis lectus egestas, sagittis tortor ut, consectetur est. Mauris lorem tellus, hendrerit vel ligula id, aliquam tincidunt eros. Nullam pellentesque gravida nisi, non tincidunt quam.
+					</p>
+					<p>
+						Nullam aliquam mollis ipsum, in pellentesque lorem commodo eget. Sed lobortis est eget sollicitudin dapibus. Aenean dolor turpis, rhoncus sit amet est a, hendrerit faucibus magna. Nulla tortor erat, hendrerit ac varius at, vestibulum id nisi. Nam dapibus pharetra gravida. Proin non tincidunt sapien. Mauris dapibus tempus sollicitudin. In fermentum a ipsum eget aliquam. Nulla rhoncus lorem in leo scelerisque euismod. Aliquam lacinia tempor justo et posuere. In ac velit mattis, vulputate velit vel, dictum orci. Sed ac finibus massa.
+					</p>
+				</div>
+				<img alt="noimg" src="/static/img/noimg.svg">
 			</article>
 		</div>
 	</main>
