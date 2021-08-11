00e7b952822dd388745df05161ac4640c2fcae08
Ilia <kul.2002.il@gmail.com>
2021-08-11 18:52:11 +0700

    Сделан витжет-переключатель.

---
+++ view/about.php
@@ -0,0 +1,42 @@
+<div class="container row">
+	<div class="col-md-3 col-lg-2">
+		<div class="d-flex flex-column flex-shrink-0 p-3" style="width: 280px;">
+			<ul class="nav nav-pills flex-column mb-auto">
+				<li>
+					<label class="p-2"
+						for="page1">
+						Страница 1
+					</label>
+				</li>
+				<li>
+					<label class="p-2"
+						for="page2">
+						Страница 2
+					</label>
+				</li>
+				<li>
+					<label class="p-2"
+						for="page3">
+						Страница 3
+					</label>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="switch-widget col">
+		<input type="radio" name="pager" id="page1" checked>
+		<div>
+			Страница 1
+		</div>
+		
+		<input type="radio" name="pager" id="page2">
+		<div>
+			Страница 2
+		</div>
+		
+		<input type="radio" name="pager" id="page3">
+		<div>
+			Страница 3
+		</div>
+	</div>
+</div>
